83252a7845a1c7186c8306ca0d88d5bff2000a58
Mace <masongurtler@gmail.com>
2020-02-11 13:08:22 -0500

    Fixed Movement, Added Atomic pickup
    Fixed player movement to no longer drift, added Atomic Currency pickup but I still need to finish it

--- UFO_Tester/Assets/_Completed-Assets/Scripts/Movement.cs
+++ UFO_Tester/Assets/_Completed-Assets/Scripts/Movement.cs
@@ -1,23 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
-
-public class Movement : MonoBehaviour
-{
-	public float speed;
-
-	private Rigidbody2D rb2d;
-
-	void Start()
-	{
-		rb2d = GetComponent<Rigidbody2D> ();
-	}
-
-	void FixedUpdate() 
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
-		Vector2 movement = new Vector2 (moveHorizontal,moveVertical);
-		rb2d.AddForce (movement * speed);
-	}
+using System.Collections;
+ 
+public class BasicMovement : MonoBehaviour {
+ 
+    public float speed = 10f;
+   
+    void Update () {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+ 
+        gameObject.GetComponent<CharacterController>().Move(transform.TransformDirection(input * speed * Time.deltaTime));
+    }
 }
