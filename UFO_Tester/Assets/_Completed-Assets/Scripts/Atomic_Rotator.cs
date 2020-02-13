2b5b298c05a472acaa15b3183550806b48b4b81e
Mace <masongurtler@gmail.com>
2020-02-13 13:26:49 -0500

    fixed movement and walls, added pickup types

---
+++ UFO_Tester/Assets/_Completed-Assets/Scripts/Atomic_Rotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Atomic_Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate (new Vector3 (0,0,5) * Time.deltaTime);
+    }
+}
