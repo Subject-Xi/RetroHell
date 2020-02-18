3bb63938dc7eb713c3210c8dd0fa151c764be44f
Mason Gurtler <masongurtler@gmail.com>
2020-02-18 09:22:55 -0500

    Pickups can now be collected
    Gold and Atomic crystals can now be collected, sheild and health crystals still need to be added

--- UFO_Tester/Assets/_Completed-Assets/Scripts/CC_Move2.cs
+++ UFO_Tester/Assets/_Completed-Assets/Scripts/CC_Move2.cs
@@ -2,16 +2,25 @@
  using System.Collections.Generic;
  using Prime31;
  using UnityEngine;
+ using UnityEngine.UI;
   
  public class CC_Move2 : MonoBehaviour {
   
      CharacterController characterController;
      bool canPlayerMove = true;
      public float speed = 0.0f;
+     private int GoldCount;
+     public Text GoldCountText;
+     private int AtomicCount;
+     public Text AtomicCountText;
   
   	 void Start()
   	 {
   	 	characterController = GetComponent<CharacterController>();
+  	 	GoldCount = 0;
+  	 	AtomicCount = 0;
+  	 	SetGoldCountText();
+  	 	SetAtomicCountText();
   	 }
 
      void FixedUpdate()
@@ -40,5 +49,30 @@
      	{
      		return;
      	}
+     }
+
+     void OnTriggerEnter(Collider other)
+     {
+     	if (other.gameObject.CompareTag("Gold"))
+     	{
+     		Destroy(other.gameObject);
+     		GoldCount = GoldCount + 1;
+     		SetGoldCountText();
+     	}
+     	if (other.gameObject.CompareTag("Atomic_Crystal"))
+     	{
+     		Destroy(other.gameObject);
+     		AtomicCount = AtomicCount + 1;
+     		SetAtomicCountText();
+     	}
+     }
+
+     void SetGoldCountText()
+     {
+     	GoldCountText.text = "Gold: " + GoldCount.ToString();
+     }
+     void SetAtomicCountText()
+     {
+     	AtomicCountText.text = "Atomic Crystals: " + AtomicCount.ToString();
      }
  }
