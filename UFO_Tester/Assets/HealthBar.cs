fb69961f69e7dfaf4ffb3740acb8bda1bdb2112d
Mace <masongurtler@gmail.com>
2020-02-18 13:24:47 -0500

    Changed some Pickup stuff
    edited some of the pickup stuff

---
+++ UFO_Tester/Assets/HealthBar.cs
@@ -0,0 +1,50 @@
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using Prime31;
+ using UnityEngine;
+ using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+	 private int MaxHealth;
+     private int HealthCount;
+     private float HealthBarLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 100;
+  	 	HealthCount = 85;
+  	 	HealthBarLength = Screen.width/5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AdjustHealthCount(0);
+    }
+
+    void OnGUI()
+    {
+     	GUI.Box(new Rect(10, 10, HealthBarLength, 20), HealthCount + "/" + MaxHealth);
+    }
+
+    public void AdjustHealthCount (int adj)
+    {
+
+   HealthCount += adj;
+   if(HealthCount < 0)
+   {
+        HealthCount = 0;
+   }
+   if(HealthCount > MaxHealth)
+   {
+        HealthCount = MaxHealth;
+   }
+   if(MaxHealth < 1)
+   {
+        MaxHealth = 1;
+   }
+   HealthBarLength = (Screen.width / 2) * (HealthCount / (float)MaxHealth);
+	}
+}
--- UFO_Tester/Assets/_Completed-Assets/Scripts/CC_Move2.cs
+++ UFO_Tester/Assets/_Completed-Assets/Scripts/CC_Move2.cs
@@ -11,16 +11,33 @@
      public float speed = 0.0f;
      private int GoldCount;
      public Text GoldCountText;
-     private int AtomicCount;
+     private float AtomicMax;
+     private float AtomicCount;
      public Text AtomicCountText;
+     private int MaxHealth;
+     private int HealthCount;
+     public Text HealthCountText;
+     private int MaxSheild;
+     private int SheildCount;
+     public Text SheildCountText;
   
   	 void Start()
   	 {
   	 	characterController = GetComponent<CharacterController>();
   	 	GoldCount = 0;
+  	 	AtomicMax = 1000000;
   	 	AtomicCount = 0;
+  	 	MaxHealth = 100;
+  	 	HealthCount = 85;
+  	 	MaxSheild = 100;
+  	 	SheildCount = 85;
   	 	SetGoldCountText();
   	 	SetAtomicCountText();
+  	 	SetHealthBarText();
+  	 	SetSheildBarText();
+
+  	 	//Enable Dev shenanigans
+  	 	//JustForFun();
   	 }
 
      void FixedUpdate()
@@ -62,8 +79,30 @@
      	if (other.gameObject.CompareTag("Atomic_Crystal"))
      	{
      		Destroy(other.gameObject);
-     		AtomicCount = AtomicCount + 1;
+     		//AtomicCount = AtomicCount + 1;
+     		AtomicCount++;
+     		
      		SetAtomicCountText();
+     	}
+     	if (other.gameObject.CompareTag("Health_Crystal"))
+     	{
+     		Destroy(other.gameObject);
+     		HealthCount = HealthCount + 20; 
+     		if(HealthCount>MaxHealth)
+     		{
+     			HealthCount = MaxHealth;
+     		}
+     		SetHealthBarText();
+     	}
+     	if (other.gameObject.CompareTag("Sheild_Crystal"))
+     	{
+     		Destroy(other.gameObject);
+     		SheildCount = SheildCount + 20;
+     		if(SheildCount>MaxSheild)
+     		{
+     			SheildCount = MaxSheild;
+     		}
+     		SetSheildBarText();
      	}
      }
 
@@ -74,5 +113,31 @@
      void SetAtomicCountText()
      {
      	AtomicCountText.text = "Atomic Crystals: " + AtomicCount.ToString();
+     }
+     void SetHealthBarText()
+     {
+     	HealthCountText.text = "Health: " + HealthCount.ToString();
+     }
+     void SetSheildBarText()
+     {
+     	SheildCountText.text = "Sheild: " + SheildCount.ToString();
+     }
+
+     //Just for fun Dev settings
+     private void JustForFun()
+     {
+     	//for (AtomicCount=0;AtomicCount<AtomicMax;AtomicCount++)
+     	//	{
+     	//		AtomicCount++;
+     	//	}
+
+     	for (float i=0;i<AtomicMax;i++)
+     		{
+     			AtomicCount = AtomicCount + (i * Time.deltaTime);
+     			if (AtomicCount > AtomicMax)
+     			{
+     				AtomicCount = AtomicMax;
+     			}
+     		}
      }
  }
